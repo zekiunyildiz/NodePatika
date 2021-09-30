472cb7a169dd9de4fb772a3c8c3dc2c66bfac4e1
zekiunyildiz <zekiunyildiz@gmail.com>
2021-10-01 00:28:52 +0300

    circle

---
+++ async/circle/circle.js
@@ -0,0 +1,9 @@
+const circleArea = function (r) {
+  return Math.PI * r * r;
+};
+
+const circleCircumference = function (r) {
+  return 2 * Math.PI * r;
+};
+
+module.exports = { circleArea, circleCircumference };
